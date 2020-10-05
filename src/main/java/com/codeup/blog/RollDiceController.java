13355d16f5eb78814833a95c6156c396d2ee9233
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-05 16:24:38 -0500

    work

---
+++ src/main/java/com/codeup/blog/RollDiceController.java
@@ -0,0 +1,13 @@
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+    @Controller
+    public class RollDiceController {
+        @GetMapping("/roll-dice")
+        public String rollDice() {
+            return " roll-dice";
+        }
+}
+
