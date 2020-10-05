9aae9c127a3de1297307b89e7efc603134508365
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-05 10:49:08 -0500

    spring introduction

---
+++ src/main/java/com/codeup/blog/IncrementController.java
@@ -0,0 +1,15 @@
+package com.codeup.blog;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class IncrementController {
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
