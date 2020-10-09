d8099595996bdbf53419f4511293f485b38f633f
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-09 09:56:30 -0500

    user update

--- src/main/java/com/codeup/blog/models/User.java
+++ src/main/java/com/codeup/blog/models/User.java
@@ -1,4 +1,20 @@
 package com.codeup.blog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    public User() {
+    }
