fc238b13b7f0ab5741174a3467ab07e7cd05e2da
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-09 10:06:56 -0500

    user update

--- src/main/java/com/codeup/blog/models/User.java
+++ src/main/java/com/codeup/blog/models/User.java
@@ -1,6 +1,7 @@
 package com.codeup.blog.models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -16,5 +17,52 @@ public class User {
     @Column(nullable = false)
     private String username;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Ad> ads;
+
     public User() {
     }
+
+    public User(long id, String email, String username, List<Ad> ads) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.ads = ads;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Ad> getAds() {
+        return ads;
+    }
+
+    public void setAds(List<Ad> ads) {
+        this.ads = ads;
+    }
+}
+
+
+
+
